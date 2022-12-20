0b74b78eafa0886145859df8d022ba2769072203
Kara Howes <karahowes@Karas-MacBook-Air.local>
2022-12-20 18:16:44 +0100

    feat:inital setup of weather app with baseurl

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import { BASE_FORECAST_URL } from './utils/urls';
 
 export const App = () => {
+  const [location, setLocation] = useState([]);
+  const [details, setDetails] = useState('');
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    // fetch(`${BASE_FORECAST_URL}?q=${location}`)
+    fetch(BASE_FORECAST_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setDetails(data);
+        console.log(data);
+      });
+  };
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <>
+      <form onSubmit={onFormSubmit}>
+        <h2>Search location:</h2>
+        <select
+          id="locationInput"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}>
+          <option disabled value="">
+            Location:
+          </option>
+          <option value="Zurich">Zurich</option>
+          <option value="Basel">Basel</option>
+          <option value="Geneva">Geneva</option>
+          <option value="Bern">Bern</option>
+          <option value="Luzern">Luzern</option>
+          <option value="Lugano">Lugano</option>
+        </select>
+
+        <button type="submit">Submit</button>
+      </form>
+      <div> city: {details.name}  </div>
+      <div> temp: {details.main.temp}  </div>
+    </>
+  );
+};
