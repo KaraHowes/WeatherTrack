6735b435d4453cf48909d7751545c980d4303489
Kara Howes <karahowes@Karas-MacBook-Air.local>
2022-12-20 20:19:42 +0100

    feat: now have start point and destination successfully achieved

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { useState } from 'react';
 
-import { BASE_FORECAST_URL } from './utils/urls';
+// import { BASE_FORECAST_URL } from './utils/urls';
 
 export const App = () => {
-  const [location, setLocation] = useState([]);
-  const [details, setDetails] = useState('');
+  const [locationStart, setLocationStart] = useState([]);
+  const [locationEnd, setLocationEnd] = useState([]);
+  const [detailsStart, setDetailsStart] = useState('');
+  const [detailsEnd, setDetailsEnd] = useState('');
   const onFormSubmit = (event) => {
     event.preventDefault();
     // fetch(`${BASE_FORECAST_URL}?q=${location}`)
-    fetch(BASE_FORECAST_URL)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationStart}&units=metric&APPID=ad0eb5f0c488721a69e3cb4d4cef6ea9`)
       .then((res) => res.json())
       .then((data) => {
-        setDetails(data);
+        setDetailsStart(data);
         console.log(data);
+      });
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationEnd}&units=metric&APPID=ad0eb5f0c488721a69e3cb4d4cef6ea9`)
+      .then((res) => res.json())
+      .then((data) => {
+        setDetailsEnd(data);
+      // console.log(data);
       });
   };
   return (
     <>
       <form onSubmit={onFormSubmit}>
-        <h2>Search location:</h2>
+        <h2>Start:</h2>
         <select
-          id="locationInput"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}>
+          id="locationStartInput"
+          value={locationStart}
+          onChange={(e) => setLocationStart(e.target.value)}>
           <option disabled value="">
             Location:
           </option>
@@ -34,10 +42,28 @@ export const App = () => {
           <option value="Lugano">Lugano</option>
         </select>
 
+        <h2>Destination:</h2>
+        <select
+          id="locationEndInput"
+          value={locationEnd}
+          onChange={(e) => setLocationEnd(e.target.value)}>
+          <option disabled value="">
+            Location:
+          </option>
+          <option value="manchester">manchester</option>
+          <option value="leeds">leeds</option>
+          <option value="hull">hull</option>
+          <option value="london">london</option>
+          <option value="bradford">bradford</option>
+          <option value="birmingham">birmingham</option>
+        </select>
         <button type="submit">Submit</button>
       </form>
-      <div> city: {details.name}  </div>
-      <div> temp: {details.main.temp}  </div>
+      <div> Starter city: {detailsStart.name}  </div>
+      <div> temperature: {detailsStart.main.temp}  </div>
+
+      <div> Destination city: {detailsEnd.name}  </div>
+      <div> temperature: {detailsEnd.main.temp}  </div>
     </>
   );
 };
--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,3 +1,3 @@
-// export const BASE_FORECAST_URL = (cityslug) => `https://api.openweathermap.org/data/2.5/forecast?q${cityslug}&units=metric&APPID=ad0eb5f0c488721a69e3cb4d4cef6ea9`
+export const BASE_FORECAST_URL = (cityslug) => `https://api.openweathermap.org/data/2.5/forecast?q=${cityslug}&units=metric&APPID=ad0eb5f0c488721a69e3cb4d4cef6ea9`
 
-export const BASE_FORECAST_URL = 'https://api.openweathermap.org/data/2.5/weather?q=geneva&units=metric&APPID=00ceff8163f7cba27d66b6501ce70e06'
+// export const BASE_FORECAST_URL = 'https://api.openweathermap.org/data/2.5/weather?q=geneva&units=metric&APPID=00ceff8163f7cba27d66b6501ce70e06'
