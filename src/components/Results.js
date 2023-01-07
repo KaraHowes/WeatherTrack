2c7309b55af3184d8c0df41ec50a0f3df6531d57
Kara Howes <karahowes@Karas-MacBook-Air.local>
2023-01-07 22:17:53 +0100

    feat: converted temperature string into integer and then rounded to zero decimal places

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,19 +5,21 @@ import { useSelector } from 'react-redux';
 const Results = () => {
   const startCity = useSelector((store) => store.search.startCityName);
   const startTemp = useSelector((store) => store.search.startTemp);
+  const numberStartTemp = Number(startTemp).toFixed(0) // needed to convert the string into a number
   const startDesc = useSelector((store) => store.search.startDescription);
   const endCity = useSelector((store) => store.search.endCityName);
   const endTemp = useSelector((store) => store.search.endTemp);
+  const numberEndTemp = Number(endTemp).toFixed(0)
   const endDesc = useSelector((store) => store.search.endDescription);
 
   return (
     <>
       <div> Starter city: {startCity} </div>
-      <div> temperature: {startTemp} </div>
+      <div> temperature: {numberStartTemp} </div>
       <div> description: {startDesc} </div>
 
       <div> Destination city: {endCity} </div>
-      <div> temperature: {endTemp} </div>
+      <div> temperature: {numberEndTemp} </div>
       <div> description: {endDesc} </div>
     </>
   );
